c248c98391a0afbf8bb55f1f8f3c8546c3369342
Greg <greg@vida.com>
2020-03-29 10:56:54 -0700

    clearSet()

--- constrain/Classes/BulkOperations.swift
+++ constrain/Classes/BulkOperations.swift
@@ -31,4 +31,13 @@ public extension Constraints {
         return isActive ? deactivate() : activate()
     }
     
+    @discardableResult
+    func clearSet() -> Self {
+        deactivate() // this also removes them from the view
+        allConstraints = []
+        constraints = [:]
+        isActive = true
+        return self
+    }
+    
 }
